ee78da441ef473bd556fca76066eb20d2baae2dc
Ryan Roemer <ryan.roemer@formidable.com>
2018-10-15 16:42:34 -0700

    WIP: Experiments

--- scenarios/new-webpack/old-npm/webpack.config.js
+++ scenarios/new-webpack/old-npm/webpack.config.js
@@ -1,4 +1,6 @@
 const { resolve } = require("path");
+const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
+const { DuplicatesPlugin } = require("/Users/rye/scm/fmd/inspectpack/plugin");
 
 module.exports = {
   mode: "development",
@@ -11,5 +13,9 @@ module.exports = {
     path: resolve(__dirname, "dist"),
     pathinfo: true,
     filename: "[name].js"
-  }
+  },
+  plugins: [
+    new DuplicatePackageCheckerPlugin(),
+    new DuplicatesPlugin()
+  ]
 };
